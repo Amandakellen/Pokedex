79cdf86c138b4d49be9d5e5e3d0a8516db9aeccd
Amanda Soares <amanda.soares@ifood.com.br>
2025-05-14 11:34:02 -0300

    [Create_account] Add viewModel in module

--- features/create_account/src/main/java/com/example/create_account/di/createAccountModule.kt
+++ features/create_account/src/main/java/com/example/create_account/di/createAccountModule.kt
@@ -3,7 +3,10 @@ package com.example.create_account.di
 import com.example.create_account.data.datasource.remote.FirebaseAuthDataSource
 import com.example.create_account.data.repository.CreateAccountRepositoryImpl
 import com.example.create_account.domain.repository.CreateAccountRepository
+import com.example.create_account.presentation.viewmodel.CreateAccountViewModel
 import com.google.firebase.auth.FirebaseAuth
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -12,4 +15,5 @@ val createAccountModule = module{
     single { FirebaseAuth.getInstance() }
     single{ FirebaseAuthDataSource(get()) }
     factoryOf(::CreateAccountRepositoryImpl) bind CreateAccountRepository::class
+    viewModelOf(::CreateAccountViewModel)
 }
