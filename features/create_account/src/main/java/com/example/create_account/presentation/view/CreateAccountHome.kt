b25321e6d712b2a952b31b8308cb8e5c69ebf24c
Amanda Soares <amanda.soares@ifood.com.br>
2025-06-25 11:03:34 -0300

    [Create Account] Create CreateAccountHome

--- features/create_account/src/main/java/com/example/create_account/presentation/view/CreateAccountHome.kt
+++ features/create_account/src/main/java/com/example/create_account/presentation/view/CreateAccountHome.kt
@@ -1,14 +1,88 @@
 package com.example.create_account.presentation.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.create_account.R
+import com.example.design_system.components.toolBar.ToolBar
+import com.example.design_system.theme.PokedexTheme
 
 @Composable
-fun CreateAccountHome(navController: NavController) {
-    Box(modifier = Modifier) {
-        Text("Text")
+internal fun CreateAccountHome(navController: NavController) {
+    Scaffold(
+        topBar = {
+            ToolBar(
+                title = stringResource(R.string.create_account_tool_bar_title),
+                onBackPressed = { navController.popBackStack() }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateAccountContent(
+                    Modifier
+                        .weight(2f)
+                        .padding(PokedexTheme.padding.medium)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+internal fun CreateAccountContent(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(3f)
+                .fillMaxWidth(),
+            painter = painterResource(id = R.drawable.ic_girl),
+            contentDescription = null
+        )
+
+//        Text(
+//            modifier = Modifier
+//                .align(Alignment.CenterHorizontally)
+//                .padding(PokedexTheme.padding.small),
+//            text = stringResource(R.string.onboarding_screen_title),
+//            style = AppTypography.headlineMedium,
+//            textAlign = TextAlign.Center,
+//            color = PokedexTheme.text
+//        )
+//
+//        Text(
+//            modifier = Modifier
+//                .align(Alignment.CenterHorizontally)
+//                .padding(PokedexTheme.padding.small),
+//            text = stringResource(R.string.onboarding_screen_subtitle),
+//            style = AppTypography.bodyLarge,
+//            textAlign = TextAlign.Center,
+//            color = PokedexTheme.text
+//        )
     }
+}
+
+@PreviewLightDark()
+@Composable
+internal fun CreateAccountHomePreview() {
+    val navController = rememberNavController()
+    CreateAccountHome(navController = navController)
+
 }
