359263750a5e4707fbc0baffe1ed44b75c23aeec
Amanda Soares <amanda.soares@ifood.com.br>
2026-01-06 14:53:51 -0300

    [KMP] Att build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Android
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+
+    // Kotlin
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+
+    // Compose
+    alias(libs.plugins.compose.compiler) apply false
+
+    // Google Services (Android only)
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
