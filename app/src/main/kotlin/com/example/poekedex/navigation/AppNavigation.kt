24687b9395abbf45f4fee1cf01c8733f0bcfa8c1
Amanda Soares <amanda.soares@ifood.com.br>
2026-01-20 16:04:36 -0300

    [KMP] Migrate onboarding to androidMain module

--- app/src/main/kotlin/com/example/poekedex/navigation/AppNavigation.kt
+++ app/src/main/kotlin/com/example/poekedex/navigation/AppNavigation.kt
@@ -1,13 +1,10 @@
 package com.example.poekedex.navigation
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.create_account.navigation.CreateAccountNavigation
-import com.example.features.onboarding.presentation.OnboardingHomeScreen
 import com.example.onboarding.navigation.OnboardingNavigation
 
 @Composable
