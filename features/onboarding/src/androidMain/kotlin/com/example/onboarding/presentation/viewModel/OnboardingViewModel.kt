4dff61d7c0f241dab39787f69c5fdc93d8d9bf17
Amanda Soares <amanda.soares@ifood.com.br>
2026-01-20 16:02:56 -0300

    [KMP] Migrate onboarding to androidMain module

---
+++ features/onboarding/src/androidMain/kotlin/com/example/onboarding/presentation/viewModel/OnboardingViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.onboarding.presentation.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.features.onboarding.presentation.state.OnboardingState
+import com.example.onboarding.presentation.action.OnboardingAction
+import com.example.onboarding.presentation.effect.OnboardingEffect
+import com.example.onboarding.presentation.view.informative.FIRST_STEP
+import com.example.onboarding.presentation.view.informative.PAGER_SIZE
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class OnboardingViewModel : ViewModel(), OnboardingAction {
+    private val _state = MutableStateFlow<OnboardingState>(OnboardingState.Loading)
+    val state = _state
+
+    private val _effect = MutableStateFlow<OnboardingEffect?>(null)
+    val effect = _effect
+
+    override fun sendAction(action: OnboardingAction.Action) {
+        when (action) {
+            OnboardingAction.Action.GoToInformativeScreen -> {
+                _effect.value = OnboardingEffect.GoToOnboarding
+                _state.value = OnboardingState.Initial(
+                    checkNotNull(_state.value).uiModel.copy(
+                        currentStep = 0
+                    )
+                )
+            }
+
+            is OnboardingAction.Action.NavigateStep -> {
+                if (action.step in FIRST_STEP until PAGER_SIZE){
+                    _state.value =
+                        OnboardingState.Initial(
+                            checkNotNull(_state.value).uiModel.copy(
+                                currentStep = action.step
+                            )
+                        )
+                    _effect.value = OnboardingEffect.GoToStep(action.step)
+                }
+                if(action.step == PAGER_SIZE){
+                    _effect.value = OnboardingEffect.GoToLoginScreen
+                }
+
+            }
+
+            OnboardingAction.Action.ClickCreateAccountButton -> {
+                _effect.value = OnboardingEffect.GoToCreateAccount
+            }
+
+            OnboardingAction.Action.ClickLoginButton -> {
+                _effect.value = OnboardingEffect.GoToLogin
+            }
+
+            OnboardingAction.Action.Initialize -> {
+                _state.value = OnboardingState.Loading
+            }
+        }
+    }
+}
