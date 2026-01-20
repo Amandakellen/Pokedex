79ef7fe3eb1c3edae5bff5564edad15b7f885d6f
Amanda Soares <amanda.soares@ifood.com.br>
2026-01-20 15:43:40 -0300

    [KMP] Move to AndroidMaind module

--- design_system/src/androidMain/kotlin/com/example/design_system/components/toolBar/ToolBar.kt
+++ design_system/src/androidMain/kotlin/com/example/design_system/components/toolBar/ToolBar.kt
@@ -3,12 +3,11 @@ package com.example.design_system.components.toolBar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,7 +34,7 @@ fun ToolBar(
         Icon(
             painter = painterResource(id = R.drawable.arrow_black),
             contentDescription = "Back",
-            tint = androidx.compose.material3.MaterialTheme.colorScheme.onSurfaceVariant,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant,
             modifier = Modifier
                 .align(Alignment.CenterStart)
                 .padding(PokedexTheme.padding.medium)
