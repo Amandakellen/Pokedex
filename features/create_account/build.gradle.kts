df1158ffabd026b83585f58f0a4a58e309244879
Amanda Soares <amanda.soares@ifood.com.br>
2026-01-20 16:10:19 -0300

    [KMP] migrate create_account to Kotlin Multiplatform

--- features/create_account/build.gradle.kts
+++ features/create_account/build.gradle.kts
@@ -1,67 +1,80 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.example.create_account"
     compileSdk = 35
+}
 
-    defaultConfig {
-        minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.androidx.core.ktx)
+            implementation(libs.androidx.appcompat)
+            implementation(libs.material)
+            implementation(libs.ui.tooling.preview.android)
+
+            // Compose
+            implementation(libs.androidx.foundation)
+            implementation(libs.androidx.material3.android)
+
+
+            // Navigation
+            implementation(libs.androidx.navigation.compose.v260)
+
+            // Koin
+            implementation(libs.koin.core)
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose.v350)
+
+            // Firebase
+            implementation(libs.firebase.auth)
+
+            // Projeto
+            implementation(projects.designSystem)
+            implementation(projects.infrastructure.domain.coroutines)
+        }
+
+        commonMain.dependencies {
+            implementation(libs.koin.core)
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+
+            }
+        }
+
+        androidUnitTest.dependencies {
+            implementation("org.jetbrains.kotlin:kotlin-test")
+
+        }
+
+        iosTest.dependencies {
+            implementation("org.jetbrains.kotlin:kotlin-test")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+        }
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.ui.tooling.preview.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    // Compose
-    implementation(libs.androidx.foundation)
-    implementation(libs.androidx.material3.android)
-
-
-    // Navigation
-    implementation(libs.androidx.navigation.compose.v260)
-
-    // Koin
-    implementation(libs.koin.core)
-    implementation(libs.koin.android)
-    implementation(libs.koin.androidx.compose.v350)
-
-    // Firebase
-    implementation(libs.firebase.auth)
-
-    // Projeto
-    implementation(projects.designSystem)
-    implementation(projects.infrastructure.domain.coroutines)
 }
