c8478f3ff30344c3101f6b4e1837eee621be4b20
amandakellensoares <amandakellensoares@hotmail.com>
2026-09-24 18:59:32 -0300

    [KMP] delete old module

--- legacy_infrastructure/domain/coroutines/src/main/java/com/example/coroutines/usecase/extensions/FlowUseCaseExt.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coroutines.usecase.extensions
-
-import com.example.coroutines.usecase.FlowUseCase
-import kotlinx.coroutines.flow.Flow
-
-fun <T> FlowUseCase<Unit, T>.invoke(): Flow<T> = invoke(Unit)
